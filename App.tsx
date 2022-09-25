cf6634ac43fc0b6dd4c0746646e35337a12a7370
jayjaybillings <jayjaybillings@gmail.com>
2022-09-25 12:20:03 -0400

    Fixed horizontal scaling

--- App.tsx
+++ App.tsx
@@ -1,33 +1,38 @@
 import * as React from 'react';
-import './style.css';
-import GridLayout from 'react-grid-layout';
+import Card from "./components/Card";
+import { Responsive, WidthProvider } from 'react-grid-layout';
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 export default class App extends React.Component {
   render() {
     // layout is an array of objects, see the demo for more complete usage
-    const layout = [
-      { i: 'a', x: 0, y: 0, w: 1, h: 2, static: true },
-      { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-      { i: 'c', x: 4, y: 0, w: 1, h: 2 },
-    ];
+    const layout = { lg : [
+      { i: 'a', x: 0, y: 0, w: 4, h: 1 },
+      { i: 'b', x: 4, y: 0, w: 4, h: 1 },
+      { i: 'c', x: 8, y: 0, w: 4, h: 1 },
+    ]};
     return (
-      <GridLayout
-        className="layout"
-        layout={layout}
-        cols={12}
-        rowHeight={30}
-        width={1200}
-      >
-        <div key="a" style={{ backgroundColor: 'yellow' }}>
-          a
-        </div>
-        <div key="b" style={{ backgroundColor: 'red' }}>
-          b
-        </div>
-        <div key="c" style={{ backgroundColor: 'green' }}>
-          c
-        </div>
-      </GridLayout>
+      <div>
+        <ResponsiveGridLayout
+          className="layout"
+          layouts={layout}
+          breakpoints={{ lg: 1200 }}
+          cols={{ lg: 12 }}
+          rowHeight={281}
+          width={1200}
+        >
+          <div key="a" style={{ backgroundColor: 'yellow' }}>
+            <Card label="a"/>
+          </div>
+          <div key="b" style={{ backgroundColor: 'red' }}>
+            <Card label="b"/>
+          </div>
+          <div key="c" style={{ backgroundColor: 'green' }}>
+            <Card label="c"/>
+          </div>
+        </ResponsiveGridLayout>
+      </div>
     );
   }
 }
